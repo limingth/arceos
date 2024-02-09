8a1475ed619dc371b0aa735bacde514b7d3f1927
dbydd <1992003927@qq.com>
2024-02-09 14:03:00 +0800

    还是先把rust的部分写好罢

--- crates/driver_usb/src/host/mod.rs
+++ crates/driver_usb/src/host/mod.rs
@@ -1,15 +1,10 @@
+pub mod bindings;
 pub mod xhci;
 use driver_common::BaseDriverOps;
 
 /// The information of the graphics device.
 #[derive(Debug, Clone, Copy)]
-pub struct USBHostInfo {
-
-}
-
-
+pub struct USBHostInfo {}
 
 /// Operations that require a graphics device driver to implement.
-pub trait USBHostDriverOps: BaseDriverOps {
-
-}
+pub trait USBHostDriverOps: BaseDriverOps {}
