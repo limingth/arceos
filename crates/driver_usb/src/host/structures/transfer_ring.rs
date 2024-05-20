c3e8098a87ed894b6e8e601b095336a27eef74b5
dbydd <1992003927@qq.com>
2024-05-20 20:39:53 +0800

    需要修改传输环的入队

---
+++ crates/driver_usb/src/host/structures/transfer_ring.rs
@@ -0,0 +1,83 @@
+use core::{char::REPLACEMENT_CHARACTER, marker::PhantomData, mem};
+
+use alloc::vec::Vec;
+use axhal::mem::VirtAddr;
+use futures_util::stream::All;
+use log::{debug, info};
+use page_box::PageBox;
+use xhci::ring::trb::{self, event::Allowed, Link};
+
+use crate::host::structures::XHCI_LINK_TRB_CONTROL_TC;
+
+use super::{registers, XHCI_CONFIG_RING_SIZE, XHCI_TRB_CONTROL_C};
+
+pub type TypeXhciTrb = [u32; 4];
+
+pub struct TransferRing {
+    ring: PageBox<[[u32; 4]]>,
+    enque_index: usize,
+    deque_index: usize,
+    cycle_state: u32,
+}
+
+impl TransferRing {
+    pub fn new() -> Self {
+        super::registers::handle(|r| {
+            let transfer_ring = TransferRing {
+                ring: PageBox::new_slice([0 as u32; 4], XHCI_CONFIG_RING_SIZE), //TODO 此处写死256，后续可更改
+                enque_index: 0,
+                deque_index: 0,
+                cycle_state: XHCI_TRB_CONTROL_C as u32,
+            };
+
+            debug!("created transfer ring!");
+
+            transfer_ring
+        })
+    }
+
+    pub fn get_trb_count(&self) -> usize {
+        self.ring.len()
+    }
+
+    pub fn get_ring_addr(&self) -> VirtAddr {
+        self.ring.virt_addr()
+    }
+
+    pub fn get_deque_trb(&self) -> Option<Allowed> {
+        assert!(self.deque_index < self.get_trb_count());
+        let xhci_trb = self.ring[self.deque_index];
+        if (xhci_trb[3] & XHCI_TRB_CONTROL_C as u32) != self.cycle_state {
+            return None;
+        }
+
+        Allowed::try_from(xhci_trb).ok()
+    }
+
+    pub fn get_enque_trb(&self) -> Option<Allowed> {
+        assert!(self.enque_index < self.get_trb_count());
+        let xhci_trb = self.ring[self.enque_index];
+        if (xhci_trb[3] & XHCI_TRB_CONTROL_C as u32) == self.cycle_state {
+            return None;
+        }
+
+        Allowed::try_from(xhci_trb).ok()
+    }
+
+    pub fn inc_deque(&mut self) -> Option<Allowed> {
+        assert!(self.deque_index < self.get_trb_count());
+        assert_eq!(
+            self.ring[self.enque_index][3] & XHCI_TRB_CONTROL_C as u32,
+            self.cycle_state
+        );
+
+        self.deque_index += 1;
+
+        if self.deque_index == self.get_trb_count() {
+            self.deque_index = 0;
+            self.cycle_state ^= XHCI_TRB_CONTROL_C as u32;
+        }
+
+        Allowed::try_from(self.ring[self.deque_index]).ok()
+    }
+}
