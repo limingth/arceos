c135f08ca287451b3dffd4b2f44b0a87814b7eeb
ray <dannyray0364@gmail.com>
2024-07-03 04:01:31 -0400

    修改了OLED屏幕的地址

--- crates/driver_i2c/src/lib.rs
+++ crates/driver_i2c/src/lib.rs
@@ -77,144 +77,143 @@ unsafe fn i2c_receive_data(mio_base: u32, buffer: &mut [u8]) {
     }
 }
 
-// unsafe fn oled_fill_screen(mio_base: u32) {
-//     for y in 0..8 {
-//         oled_set_cursor(mio_base, y, 0);
-//         for x in 0..128 {
-//             oled_write_data(mio_base, 0xFF);  // 0xFF表示全亮
-//         }
-//     }
-// }
+unsafe fn oled_fill_screen(mio_base: u32) {
+    for y in 0..8 {
+        oled_set_cursor(mio_base, y, 0);
+        for x in 0..128 {
+            oled_write_data(mio_base, 0xFF);  // 0xFF表示全亮
+        }
+    }
+}
 
-// // OLED 特定的命令和数据发送函数
-// unsafe fn oled_i2c_send_byte(mio_base: u32, byte: u8) {
-//     debug!("Sending OLED byte {:#X}", byte);
-//     i2c_send_data(mio_base, &[byte]);
-// }
+// OLED 特定的命令和数据发送函数
+unsafe fn oled_i2c_send_byte(mio_base: u32, byte: u8) {
+    debug!("Sending OLED byte {:#X}", byte);
+    i2c_send_data(mio_base, &[byte]);
+}
 
-// unsafe fn oled_write_command(mio_base: u32, command: u8) {
-//     debug!("Writing OLED command {:#X}", command);
-//     oled_i2c_send_byte(mio_base, 0x78);  // 从机地址
-//     oled_i2c_send_byte(mio_base, 0x00);  // 写命令
-//     oled_i2c_send_byte(mio_base, command);
-// }
+unsafe fn oled_write_command(mio_base: u32, command: u8) {
+    debug!("Writing OLED command {:#X}", command);
+    oled_i2c_send_byte(mio_base, 0x3C);  // 从机地址
+    oled_i2c_send_byte(mio_base, 0x00);  // 写命令
+    oled_i2c_send_byte(mio_base, command);
+}
 
-// unsafe fn oled_write_data(mio_base: u32, data: u8) {
-//     debug!("Writing OLED data {:#X}", data);
-//     oled_i2c_send_byte(mio_base, 0x78);  // 从机地址
-//     oled_i2c_send_byte(mio_base, 0x40);  // 写数据
-//     oled_i2c_send_byte(mio_base, data);
-// }
+unsafe fn oled_write_data(mio_base: u32, data: u8) {
+    debug!("Writing OLED data {:#X}", data);
+    oled_i2c_send_byte(mio_base, 0x3C);  // 从机地址
+    oled_i2c_send_byte(mio_base, 0x40);  // 写数据
+    oled_i2c_send_byte(mio_base, data);
+}
 
-// unsafe fn oled_set_cursor(mio_base: u32, y: u8, x: u8) {
-//     debug!("Setting OLED cursor to ({}, {})", y, x);
-//     oled_write_command(mio_base, 0xB0 | y);  // 设置Y位置
-//     oled_write_command(mio_base, 0x10 | ((x & 0xF0) >> 4));  // 设置X位置高4位
-//     oled_write_command(mio_base, 0x00 | (x & 0x0F));  // 设置X位置低4位
-// }
+unsafe fn oled_set_cursor(mio_base: u32, y: u8, x: u8) {
+    debug!("Setting OLED cursor to ({}, {})", y, x);
+    oled_write_command(mio_base, 0xB0 | y);  // 设置Y位置
+    oled_write_command(mio_base, 0x10 | ((x & 0xF0) >> 4));  // 设置X位置高4位
+    oled_write_command(mio_base, 0x00 | (x & 0x0F));  // 设置X位置低4位
+}
 
-// unsafe fn oled_clear(mio_base: u32) {
-//     debug!("Clearing OLED display");
-//     for j in 0..8 {
-//         oled_set_cursor(mio_base, j, 0);
-//         for _ in 0..128 {
-//             oled_write_data(mio_base, 0x00);
-//         }
-//     }
-// }
+unsafe fn oled_clear(mio_base: u32) {
+    debug!("Clearing OLED display");
+    for j in 0..8 {
+        oled_set_cursor(mio_base, j, 0);
+        for _ in 0..128 {
+            oled_write_data(mio_base, 0x00);
+        }
+    }
+}
 
-// fn get_char_font(c: char) -> [u8; 8] {
-//     match c {
-//         'h' => [0x00, 0x7F, 0x08, 0x08, 0x08, 0x08, 0x70, 0x00],
-//         'e' => [0x00, 0x3E, 0x49, 0x49, 0x49, 0x41, 0x00, 0x00],
-//         'l' => [0x00, 0x41, 0x7F, 0x40, 0x00, 0x00, 0x00, 0x00],
-//         'o' => [0x00, 0x3E, 0x41, 0x41, 0x41, 0x3E, 0x00, 0x00],
-//         'r' => [0x00, 0x7F, 0x08, 0x08, 0x08, 0x30, 0x00, 0x00],
-//         'u' => [0x00, 0x3F, 0x40, 0x40, 0x40, 0x3F, 0x00, 0x00],
-//         's' => [0x00, 0x32, 0x49, 0x49, 0x49, 0x26, 0x00, 0x00],
-//         't' => [0x00, 0x01, 0x01, 0x7F, 0x01, 0x01, 0x00, 0x00],
-//         _ => [0x00; 8], // 默认未定义字符为空
-//     }
-// }
+fn get_char_font(c: char) -> [u8; 8] {
+    match c {
+        'h' => [0x00, 0x7F, 0x08, 0x08, 0x08, 0x08, 0x70, 0x00],
+        'e' => [0x00, 0x3E, 0x49, 0x49, 0x49, 0x41, 0x00, 0x00],
+        'l' => [0x00, 0x41, 0x7F, 0x40, 0x00, 0x00, 0x00, 0x00],
+        'o' => [0x00, 0x3E, 0x41, 0x41, 0x41, 0x3E, 0x00, 0x00],
+        'r' => [0x00, 0x7F, 0x08, 0x08, 0x08, 0x30, 0x00, 0x00],
+        'u' => [0x00, 0x3F, 0x40, 0x40, 0x40, 0x3F, 0x00, 0x00],
+        's' => [0x00, 0x32, 0x49, 0x49, 0x49, 0x26, 0x00, 0x00],
+        't' => [0x00, 0x01, 0x01, 0x7F, 0x01, 0x01, 0x00, 0x00],
+        _ => [0x00; 8], // 默认未定义字符为空
+    }
+}
 
-// unsafe fn oled_show_char(mio_base: u32, line: u8, column: u8, char: char) {
-//     debug!("Showing char '{}' at line {}, column {}", char, line, column);
-//     let font = get_char_font(char);
-//     oled_set_cursor(mio_base, (line - 1) * 2, (column - 1) * 8);
-//     for i in 0..8 {
-//         oled_write_data(mio_base, font[i]);
-//     }
-//     oled_set_cursor(mio_base, (line - 1) * 2 + 1, (column - 1) * 8);
-//     for i in 0..8 {
-//         oled_write_data(mio_base, font[i]);
-//     }
-// }
+unsafe fn oled_show_char(mio_base: u32, line: u8, column: u8, char: char) {
+    debug!("Showing char '{}' at line {}, column {}", char, line, column);
+    let font = get_char_font(char);
+    oled_set_cursor(mio_base, (line - 1) * 2, (column - 1) * 8);
+    for i in 0..8 {
+        oled_write_data(mio_base, font[i]);
+    }
+    oled_set_cursor(mio_base, (line - 1) * 2 + 1, (column - 1) * 8);
+    for i in 0..8 {
+        oled_write_data(mio_base, font[i]);
+    }
+}
 
-// unsafe fn oled_show_string(mio_base: u32, line: u8, column: u8, string: &str) {
-//     debug!("Showing string '{}' at line {}, column {}", string, line, column);
-//     for (i, char) in string.chars().enumerate() {
-//         oled_show_char(mio_base, line, column + i as u8, char);
-//     }
-// }
+unsafe fn oled_show_string(mio_base: u32, line: u8, column: u8, string: &str) {
+    debug!("Showing string '{}' at line {}, column {}", string, line, column);
+    for (i, char) in string.chars().enumerate() {
+        oled_show_char(mio_base, line, column + i as u8, char);
+    }
+}
 
-// unsafe fn oled_init(mio_base: u32) {
-//     debug!("Initializing OLED at base {:#X}", mio_base);
-//     for _ in 0..1_000_000 {
-//         // 上电延时
-//     }
+unsafe fn oled_init(mio_base: u32) {
+    debug!("Initializing OLED at base {:#X}", mio_base);
+    for _ in 0..1_000_000 {
+        // 上电延时
+    }
     
-//     i2c_init(mio_base, 0x78);
-//     oled_write_command(mio_base, 0xAE);  // 关闭显示
+    i2c_init(mio_base, 0x3C);
+    oled_write_command(mio_base, 0xAE);  // 关闭显示
 
-//     oled_write_command(mio_base, 0x00);  // 设置低列地址
-//     oled_write_command(mio_base, 0x10);  // 设置高列地址
+    oled_write_command(mio_base, 0x00);  // 设置低列地址
+    oled_write_command(mio_base, 0x10);  // 设置高列地址
 
-//     oled_write_command(mio_base, 0x40);  // 设置起始行地址
+    oled_write_command(mio_base, 0x40);  // 设置起始行地址
 
-//     oled_write_command(mio_base, 0x81);  // 设置对比度控制寄存器
-//     oled_write_command(mio_base, 0xFF);
+    oled_write_command(mio_base, 0x81);  // 设置对比度控制寄存器
+    oled_write_command(mio_base, 0xFF);
 
-//     oled_write_command(mio_base, 0xA1);  // 设置段重新映射
-//     oled_write_command(mio_base, 0xA6);  // 设置正常显示
+    oled_write_command(mio_base, 0xA1);  // 设置段重新映射
+    oled_write_command(mio_base, 0xA6);  // 设置正常显示
 
-//     oled_write_command(mio_base, 0xA8);  // 设置多路复用比率
-//     oled_write_command(mio_base, 0x3F);  // 1/64占空比
+    oled_write_command(mio_base, 0xA8);  // 设置多路复用比率
+    oled_write_command(mio_base, 0x3F);  // 1/64占空比
 
-//     oled_write_command(mio_base, 0xC8);  // 设置COM输出扫描方向
+    oled_write_command(mio_base, 0xC8);  // 设置COM输出扫描方向
 
-//     oled_write_command(mio_base, 0xD3);  // 设置显示偏移
-//     oled_write_command(mio_base, 0x00);  // 无偏移
+    oled_write_command(mio_base, 0xD3);  // 设置显示偏移
+    oled_write_command(mio_base, 0x00);  // 无偏移
 
-//     oled_write_command(mio_base, 0xD5);  // 设置显示时钟分频比/振荡器频率
-//     oled_write_command(mio_base, 0x80);
+    oled_write_command(mio_base, 0xD5);  // 设置显示时钟分频比/振荡器频率
+    oled_write_command(mio_base, 0x80);
 
-//     oled_write_command(mio_base, 0xD8);  // 设置预充电周期
-//     oled_write_command(mio_base, 0x05);
+    oled_write_command(mio_base, 0xD8);  // 设置预充电周期
+    oled_write_command(mio_base, 0x05);
 
-//     oled_write_command(mio_base, 0xD9);  // 设置COM引脚硬件配置
-//     oled_write_command(mio_base, 0xF1);
+    oled_write_command(mio_base, 0xD9);  // 设置COM引脚硬件配置
+    oled_write_command(mio_base, 0xF1);
 
-//     oled_write_command(mio_base, 0xDA);  // 设置VCOMH
-//     oled_write_command(mio_base, 0x30);
+    oled_write_command(mio_base, 0xDA);  // 设置VCOMH
+    oled_write_command(mio_base, 0x30);
 
-//     oled_write_command(mio_base, 0x8D);  // 设置电荷泵
-//     oled_write_command(mio_base, 0x14);
+    oled_write_command(mio_base, 0x8D);  // 设置电荷泵
+    oled_write_command(mio_base, 0x14);
 
-//     oled_write_command(mio_base, 0xAF);  // 打开显示
-//     oled_clear(MIO1_BASE);
-// }
+    oled_write_command(mio_base, 0xAF);  // 打开显示
+    oled_clear(MIO1_BASE);
+}
 
 pub fn run_iicoled() {
+    debug!("Running I2C OLED example");
     unsafe {
         configure_mio_for_i2c(MIO1_BASE);
-        i2c_init(MIO1_BASE, 0x3C); 
-        let send_data = [0x55, 0xAA, 0xF0];
-        debug!("Sending data: {:?}", send_data);
-        i2c_send_data(MIO1_BASE, &send_data);
-        let mut receive_buffer = [0u8; 3];
-        debug!("Receiving data into buffer...");
-        i2c_receive_data(MIO1_BASE, &mut receive_buffer);
-        debug!("Received data: {:?}", receive_buffer);
+        oled_init(MIO1_BASE);
+    }
+    while true {
+        unsafe {
+            oled_fill_screen(MIO1_BASE);
+        }
     }
 }
 
@@ -223,6 +222,17 @@ pub fn run_iicoled() {
 
 
 
+// unsafe {
+//         configure_mio_for_i2c(MIO1_BASE);
+//         i2c_init(MIO1_BASE, 0x3C); 
+//         let send_data = [0x55, 0xAA, 0xF0];
+//         debug!("Sending data: {:?}", send_data);
+//         i2c_send_data(MIO1_BASE, &send_data);
+//         let mut receive_buffer = [0u8; 3];
+//         debug!("Receiving data into buffer...");
+//         i2c_receive_data(MIO1_BASE, &mut receive_buffer);
+//         debug!("Received data: {:?}", receive_buffer);
+//     }
 
 
 
@@ -232,15 +242,5 @@ pub fn run_iicoled() {
 
 
 
-// debug!("Running I2C OLED example");
-    // unsafe {
-    //     configure_mio_for_i2c(MIO1_BASE);
-    //     oled_init(MIO1_BASE);
-    // }
-    // while true {
-    //     unsafe {
-    //         oled_fill_screen(MIO1_BASE);
-    //     }
-    // }
 
 
